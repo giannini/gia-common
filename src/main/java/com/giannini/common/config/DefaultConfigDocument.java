e74c49ebf9fa93d20c3ddc6451c75c2f7619cbd1
jiangningyu <jiangningyu@10.242.53.168>
2016-06-21 16:12:18 +0800

    [config]DefaultConfigDocument实现
    除了load没有实现外，其余部分暂时先写了下（以后再改）

--- src/main/java/com/giannini/common/config/DefaultConfigDocument.java
+++ src/main/java/com/giannini/common/config/DefaultConfigDocument.java
@@ -1,25 +1,54 @@
 package com.giannini.common.config;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 缺省配置文件类
  * 
- * @author gainnini
+ * @author giannini
  */
 public class DefaultConfigDocument implements Configuration {
 
-    public long getVersion() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+    /**
+     * 日志记录
+     */
+    private static final Logger logger = LoggerFactory
+            .getLogger(DefaultConfigDocument.class);
 
-    public Long getScanMillis() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+    /**
+     * 系统属性名称,可配置conf默认路径
+     */
+    public static final String PROPERTY_CONF_HOME = "CONF_HOME";
 
-    public ElementNode getRootElement() {
-        // TODO Auto-generated method stub
-        return null;
+    /**
+     * conf文件的默认路径
+     */
+    public static final String DEFAULT_CONF_HOME = "/home/gia/conf";
+
+    /**
+     * 当前conf文件的目录
+     */
+    private String confHome = DEFAULT_CONF_HOME;
+
+    /**
+     * 配置的根节点
+     */
+    private volatile ElementNode rootNode = null;
+
+    /**
+     * 配置的版本号
+     */
+    private volatile long version = System.currentTimeMillis();
+
+    private final Set<ConfigWatcher> watcherSet = new HashSet<ConfigWatcher>();
+
+    public DefaultConfigDocument() {
+        this.confHome = System.getProperty(PROPERTY_CONF_HOME,
+                DEFAULT_CONF_HOME);
     }
 
     public void load() throws Exception {
@@ -27,19 +56,42 @@ public class DefaultConfigDocument implements Configuration {
 
     }
 
+    public long getVersion() {
+        return version;
+    }
+
+    public Long getScanMillis() {
+        return 0L;
+    }
+
+    public ElementNode getRootElement() {
+        return this.rootNode;
+    }
+
     public boolean addWatcher(ConfigWatcher watcher) {
-        // TODO Auto-generated method stub
-        return false;
+        synchronized (this.watcherSet) {
+            return this.watcherSet.add(watcher);
+        }
     }
 
     public void removeWatcher(ConfigWatcher watcher) {
-        // TODO Auto-generated method stub
-
+        synchronized (this.watcherSet) {
+            this.watcherSet.remove(watcher);
+        }
     }
 
     public void clearWatcher() {
-        // TODO Auto-generated method stub
+        synchronized (this.watcherSet) {
+            this.watcherSet.clear();
+        }
+    }
 
+    public String getConfHome() {
+        return confHome;
+    }
+
+    public void setConfHome(String confHome) {
+        this.confHome = confHome;
     }
 
 }
