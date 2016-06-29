a089c59d1293fe7ebf3731b88a9fbff1a4b449d0
Gia <gia@giannini.com>
2016-06-29 11:15:33 +0800

    [Cache] ICacheService接口定义
    定义缓存服务的通用接口

--- src/main/java/com/giannini/common/cache/EhcacheAdapter.java
+++ src/main/java/com/giannini/common/cache/EhcacheAdapter.java
@@ -1,5 +1,67 @@
 package com.giannini.common.cache;
 
+import java.util.Date;
+
 public class EhcacheAdapter implements ICacheService {
+
+    public void disposeAll() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void clear() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public boolean delete(Object key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Object get(Object key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean keyExists(Object key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Object replace(Object key, Object value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object replace(Object key, Object value, Date expiry) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public boolean set(Object key, Object value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean set(Object key, Object value, Date expiry) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public boolean set(Object key, Object value, Long liveMillis) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public Object setIfAbsent(Object key, Object value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Object setIfAbsent(Object key, Object value, Date expiry) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
--- src/main/java/com/giannini/common/cache/ICacheService.java
+++ src/main/java/com/giannini/common/cache/ICacheService.java
@@ -1,5 +1,142 @@
 package com.giannini.common.cache;
 
+import java.util.Date;
+
 public interface ICacheService {
+
+    /**
+     * 缓存服务关闭接口
+     * <p>
+     * 将所有当前缓存的数据丢弃, 并关闭服务
+     * <p>
+     * 注意: 该接口应该只供Helper类来调用
+     */
+    public void disposeAll();
+
+    /**
+     * 清除当前已经缓存的所有数据
+     */
+    public void clear();
+
+    /**
+     * 删除指定键值对
+     * 
+     * @param key
+     *            建
+     * @return true=操作成功;false=操作失败
+     */
+    public boolean delete(Object key);
+
+    /**
+     * 获取指定键对应的值
+     * 
+     * @param key
+     *            键
+     * @return 值
+     */
+    public Object get(Object key);
+
+    /**
+     * 判断指定键是否存在
+     * 
+     * @param key
+     *            键
+     * @return true=存在; false=不存在
+     */
+    public boolean keyExists(Object key);
+
+    /**
+     * 替换一个键值对, 该键值对不会过期
+     * <p>
+     * 只有当指定的键存在cache中时, 替换才会发生.
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @return object=原来被缓存的值; null=指定键不存在cache中
+     * @see #setIfAbsent(ICachedKey, Object)
+     */
+    public Object replace(Object key, Object value);
+
+    /**
+     * 替换一个键值对, 并设定过期时间
+     * <p>
+     * 只有当指定的键存在cache中时, 替换才会发生.
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @param expiry
+     *            过期时间(null=默认过期时间)
+     * @return object=原来被缓存的值; null=指定键不存在cache中
+     */
+    public Object replace(Object key, Object value, Date expiry);
+
+    /**
+     * 设置一个键值对, 该键值对不会过期
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @return true=操作成功;false=操作失败
+     */
+    public boolean set(Object key, Object value);
+
+    /**
+     * 设置一个键值对, 并设定过期时间
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @param expiry
+     *            过期时间(null=默认过期时间)
+     * @return true=操作成功;false=操作失败
+     */
+    public boolean set(Object key, Object value, Date expiry);
+
+    /**
+     * 设置一个键值对, 并设定存活时间
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @param liveMillis
+     *            存活时间(单位:毫秒, null=默认过期时间)
+     * @return true=操作成功;false=操作失败
+     */
+    public boolean set(Object key, Object value, Long liveMillis);
+
+    /**
+     * 设置一个键值对, 该键值对不会过期
+     * <p>
+     * 只有当指定的键没有被cache缓存时, 设置才能成功
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @return object=已存在的值;null=设置操作成功
+     */
+    public Object setIfAbsent(Object key, Object value);
+
+    /**
+     * 设置一个键值对, 并设定过期时间
+     * <p>
+     * 只有当指定的键没有被cache缓存时, 设置才能成功
+     * 
+     * @param key
+     *            键
+     * @param value
+     *            值
+     * @param expiry
+     *            过期时间(null=默认过期时间)
+     * @return object=已存在的值;null=设置操作成功
+     */
+    public Object setIfAbsent(Object key, Object value, Date expiry);
 
 }
